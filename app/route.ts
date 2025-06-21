cfbb9bca244230727aa3633910b6aa9ab6aed074
Jesper <jesper.l.richert@gmail.com>
2025-06-22 01:19:54 +0200

    fix: correct redirection URL in GET handler to use PAGE_HOST environment variable

--- app/route.ts
+++ app/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  return NextResponse.redirect(`${req.url}pages/hub`)
+  return NextResponse.redirect(`${process.env.PAGE_HOST}/pages/hub`)
 }
 
