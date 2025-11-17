09ac2346bdeb524c0026640d8b93720aa2163a0e
Jesper <dm@xyzjesper.dev>
2025-11-17 12:46:06 +0100

    Chnage timeout

--- lib/emailSender.ts
+++ lib/emailSender.ts
@@ -3,50 +3,48 @@
 import nodemailer from "nodemailer";
 
 const data = {
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT as unknown as number,
-    secure: process.env.EMAIL_SECURE == "true",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-    },
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT as unknown as number,
+  secure: process.env.EMAIL_SECURE == "true",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 };
 
 export async function sendEmail(name: string, from: string, text: string) {
+  const transporter = nodemailer.createTransport({
+    host: data.host,
+    port: data.port,
+    secure: data.secure,
+    auth: {
+      user: data.auth.user,
+      pass: data.auth.pass,
+    },
+    tls: {
+      rejectUnauthorized: process.env.IGNORE_TLS === "true" ? false : true,
+    },
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
+  });
 
-    const transporter = nodemailer.createTransport({
-        host: data.host,
-        port: data.port,
-        secure: data.secure,
-        auth: {
-            user: data.auth.user,
-            pass: data.auth.pass,
-        },
-        tls: {
-            rejectUnauthorized: process.env.IGNORE_TLS === "true" ? false : true,
-        },
-        connectionTimeout: 10000,
-        greetingTimeout: 10000,
-        socketTimeout: 10000,
+  try {
+    await transporter.sendMail({
+      from: `Message from xyzhub <${data.auth.user}>`,
+      to: data.auth.user,
+      subject: `New message from ${name} <${from}>`,
+      text: `Name: ${name}\nEmail: ${from}\nMessage:\n${text}`,
     });
 
-
-    try {
-        await transporter.sendMail({
-            from: `Message from xyzhub <${data.auth.user}>`,
-            to: data.auth.user,
-            subject: `New message from ${name} <${from}>`,
-            text: `Name: ${name}\nEmail: ${from}\nMessage:\n${text}`,
-        });
-
-        console.log("Email sent successfully");
-        return {success: true, message: "Email sent successfully"};
-    } catch (error) {
-        console.log("Failed to send email:", error);
-        return {
-            success: false,
-            message: "Failed to send email",
-            error: error instanceof Error ? error.message : "Unknown error"
-        };
-    }
+    console.log("Email sent successfully");
+    return { success: true, message: "Email sent successfully" };
+  } catch (error) {
+    console.log("Failed to send email:", error);
+    return {
+      success: false,
+      message: "Failed to send email",
+      error: error instanceof Error ? error.message : "Unknown error",
+    };
+  }
 }
